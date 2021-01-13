13f98729a09b1815cffcf3bd0346f837d1e8c89d
mic7x4 <mic7x4@gmail.com>
2021-01-13 19:10:22 +0200

    [feature]; cleaning the data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function formatName(user){
+    return user.firstName + " " + user.lastName;
+
+  }
+  const user = {
+     firstName:"Black",
+     lastName:"Hustle"
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{formatName(user)}</h1>
     </div>
   );
 }
