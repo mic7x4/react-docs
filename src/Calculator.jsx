2e6c7b42519c1d5728ce966770c9db36f9e64a88
mic7x4 <mic7x4@gmail.com>
2021-01-14 19:56:45 +0200

    celcius

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -18,6 +18,21 @@ export default class Calculator extends Component {
             }
             return <p>Water would not boil</p>
         }
+        function toCelsius(fahrenheit){
+            return (fahrenheit - 32) * 5 /9;
+        }
+        function toFahrenheit(celcius){
+            return (celcius * 9 /5) +32;
+        }
+        function tryConvert(temperature, convert){
+            const input = parseFloat(temperature);
+            if(Number.isNaN(input)){
+                return '';
+            }
+            const output = convert(input);
+            const rounded = Math.round(output * 1000) / 1000;
+            return rounded.toString();
+        }
         return (
             <div>
             <fieldset>
