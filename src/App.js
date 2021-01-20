09e4bb80345ad06844c25793aa7bf30a5ed492e4
mic7x4 <mic7x4@gmail.com>
2021-01-20 18:32:54 +0200

    [feature] creating tasks

--- src/App.js
+++ src/App.js
@@ -1,44 +1,31 @@
 import './App.css';
-import Calculator from './Calculator';
-import CompositionInhertance from './CompositionInhertance';
-import FilterableProductTable from './FilterableProductTable';
-import Forms from './Forms';
-import ListsandKeys from './ListsandKeys';
-import LoginControl from './LoginControl';
-import MultipleInputs from './MultipleInputs';
-import SelectTag from './SelectTag';
-import SignupDialog from './SignupDialog';
-import TextArea from './TextArea';
-import Tick from './Tick';
-import Toggle from './Toggle';
-import Welcome from './Welcome';
+import Todo from './components/Todo';
+import Form from './components/Form';
+import FilterButton from './components/FilterButton';
 
-function App(){
-  const PRODUCTS = [
-  {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
-  {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
-  {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
-  {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
-  {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
-  {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
-]; 
-console.log(PRODUCTS);
+function App({tasks}){
+  const taskList = tasks.map((task) =>
+     <Todo key={task.id} id={task.id} name={task.name} completed={task.completed} /> 
+  );
+  console.log(taskList)
   return (
-    <div className="App" style={{padding:"40px"}}>
-      {/* <Welcome name="Black Hustle" />
-      <Tick/>
-      <Toggle></Toggle>
-      <LoginControl/>
-      <ListsandKeys/> */}
-      {/* <Forms/> */}
-      {/* <TextArea/> */}
-      {/* <SelectTag/> */}
-      {/* <MultipleInputs/> */}
-      {/* <Calculator/> */}
-      {/* <CompositionInhertance/> */}
-      <SignupDialog/>
-      {/* <FilterableProductTable products={PRODUCTS}/> */}
-      
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <div className="filters btn-group stack-exception">
+       <FilterButton/>
+       <FilterButton/>
+       <FilterButton/>
+      </div>
+      <h2 id="list-heading">
+        3 tasks remaining
+      </h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
     </div>
   );
 }
